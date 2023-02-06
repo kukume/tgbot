5052fed3cdf21a9edbcca87494e0a47b284d9687
kuku <kuku@kuku.me>
2023-02-06 21:48:15 +0800

    upgrade telegram-bots to 6.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    val kotlinVersion = "1.8.0"
+    val kotlinVersion = "1.8.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.kapt") version kotlinVersion
-    id("org.springframework.boot") version "3.0.1"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
@@ -23,8 +21,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.telegram:telegrambots:6.4.0")
-    implementation("org.telegram:telegrambots-abilities:6.4.0")
+    implementation("org.telegram:telegrambots:6.5.0")
+    implementation("org.telegram:telegrambots-abilities:6.5.0")
     implementation("me.kuku:utils:2.2.3.0")
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("org.springframework.boot:spring-boot-starter-mail")
@@ -32,7 +30,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
+tasks.compileKotlin {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xcontext-receivers")
         jvmTarget = "17"
