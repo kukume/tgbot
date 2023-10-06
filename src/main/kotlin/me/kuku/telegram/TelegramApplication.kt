b6a3b7d3c0638013d1a677db8fec823624b0a340
kuku <kuku@kuku.me>
2023-10-06 21:15:19 +0800

    EnableReactiveMongoAuditing

--- src/main/kotlin/me/kuku/telegram/TelegramApplication.kt
+++ src/main/kotlin/me/kuku/telegram/TelegramApplication.kt
@@ -2,10 +2,10 @@ package me.kuku.telegram
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
 
 @SpringBootApplication
-@EnableMongoAuditing
+@EnableReactiveMongoAuditing
 class TelegramApplication
 
 fun main(args: Array<String>) {
