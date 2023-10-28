08967a93b399ed96734ccc30012f96d8fe143973
kuku <kuku@kuku.me>
2023-10-28 14:55:34 +0800

    enable kotlin k2 compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 @file:Suppress("VulnerableLibrariesLocal")
 
 plugins {
-    val kotlinVersion = "1.9.20-RC"
+    val kotlinVersion = "1.9.20-RC2"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "3.2.0-M3"
+    id("org.springframework.boot") version "3.2.0-SNAPSHOT"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
